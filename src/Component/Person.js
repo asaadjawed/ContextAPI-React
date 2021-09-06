7321be0155bd0fcbdfbc35a63f45efe7312271b5
shtainoortariq <asaadjawed70@gmail.com>
2021-09-06 16:38:38 +0500

    ApiUsingAxios

---
+++ src/Component/Person.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+
+const Person =()=>{
+
+    const [user, setUser] = useState([]);
+
+    useEffect(async () => {
+        try {
+            const user = await axios.get('https://jsonplaceholder.typicode.com/users')
+            setUser(user.data)
+        } catch (err) {
+            throw err;
+        }
+    }, [user])
+
+    console.log(user, 'use')
+
+    return(
+        <>
+        <div>
+            {user.map(user =>{
+                return(
+                    <ul>
+                    <li>
+                        {user && user[0]?.name}
+                    </li>
+                </ul>
+                )
+            })}
+
+        </div>
+
+
+        </>
+    )
+
+}
+export default Person
